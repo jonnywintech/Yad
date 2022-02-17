0f90e431c6727da045022dedf746cc4c828aae26
Nikola Stankovic <jonnywintech@gmail.com>
2022-02-17 04:01:46 +0100

    signature dishes

---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const consolidate = require("gulp-consolidate");
+const iconfont = require("gulp-iconfont");
+const postcss = require("gulp-postcss");
+const gulpStylelint = require("gulp-stylelint");
+const autoprefixer = require("autoprefixer");
+const browserSync = require("browser-sync").create();
+var runTimestamp = Math.round(Date.now() / 1000);
+
+//scss to css task
+gulp.task("scss", () => {
+  return gulp
+    .src("src/scss/style.scss")
+    .pipe(sass())
+    .pipe(postcss([autoprefixer("last 2 versions")]))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
+});
+
+//scss lint task
+gulp.task("scss-lint", () => {
+  return gulp.src("src/scss/*.scss").pipe(
+    gulpStylelint({
+      reporters: [
+        {
+          formatter: "string",
+          console: true,
+        },
+      ],
+    })
+  );
+});
+
+//iconfont task
+gulp.task("iconfont", () => {
+  return gulp
+    .src("src/svg/*.svg")
+    .pipe(
+      iconfont({
+        fontName: "iconfont",
+        formats: ["ttf", "eot", "woff", "woff2"],
+        appendCodepoints: true,
+        appendUnicode: false,
+        normalize: true,
+        fontHeight: 1000,
+        centerHorizontally: true,
+      })
+    )
+    .on("glyphs", function (glyphs, options) {
+      gulp
+        .src("src/iconfont-template/iconfont.scss")
+        .pipe(
+          consolidate("underscore", {
+            glyphs: glyphs,
+            fontName: options.fontName,
+            fontDate: new Date().getTime(),
+          })
+        )
+        .pipe(gulp.dest("src/scss/icon-font"));
+    })
+    .pipe(gulp.dest("dist/fonts"));
+});
+
+// copy js files to dist
+gulp.task("copy-js", () => {
+  return gulp.src("src/js/*.js").pipe(gulp.dest("dist/js"));
+});
+
+// copy html files to dist
+gulp.task("copy-html", () => {
+  return gulp.src("*.{html,ico}").pipe(gulp.dest("dist"));
+});
+
+// copy font files to dist
+gulp.task("copy-fonts", () => {
+  return gulp
+    .src("src/fonts/*.{ttf,woff,woff2,eof}")
+    .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task("copy-assets", () => {
+  return gulp
+    .src("src/images/*.{png,svg,jpg,jpeg}")
+    .pipe(gulp.dest("dist/images"));
+});
+
+// browser sync task
+gulp.task("watch", () => {
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+    },
+  });
+  gulp.watch("src/**/*.scss", gulp.series("scss-lint", "scss"));
+  gulp.watch("*.html", gulp.series("copy-html"));
+  gulp.watch("src/**/*.js", gulp.series("copy-js"));
+  gulp.watch("src/**/*.{png,svg,jpg,jpeg}", gulp.series("copy-assets"));
+  gulp.watch("*.html").on("change", browserSync.reload);
+  gulp.watch("src/**/*.{png,svg,jpg,jpeg}").on("change", browserSync.reload);
+  gulp.watch("src/**/*.js").on("change", browserSync.reload);
+});
+
+//build project
+gulp.task(
+  "project-build",
+  gulp.series(
+    "iconfont",
+    "copy-assets",
+    "copy-fonts",
+    "scss-lint",
+    "scss",
+    "copy-js",
+    "copy-html"
+  )
+);
+
+//to run watch task type: gulp
+gulp.task("default", gulp.series("watch"));
---
+++ src/js/main.js
@@ -0,0 +1,82 @@
+/// slider if exist ///
+
+// const swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   slidesPerGroup: 1,
+//   loop: true,
+//   loopFillGroupWithBlank: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+
+// swiper();
+
+// mobile
+const hamburger = document.querySelector(".header__hamburger");
+const moblieMenu = document.querySelector(".header__menu");
+const mobileSearchBtn = document.querySelector(".header-mobile__button");
+const mobileInput = document.querySelector(".header-mobile__input");
+const headerLinks = document.querySelectorAll(".header__link");
+
+function hamburgerOpen(){
+  hamburger.style.animation = null;
+  hamburger.classList.remove("icon-hamburger");
+  hamburger.classList.add("icon-closex");
+  hamburger.classList.toggle('animate');
+}
+
+function hamburgerClose(){
+  hamburger.style.animation = null;
+  hamburger.classList.remove("icon-closex")
+  hamburger.classList.add("icon-hamburger");
+  hamburger.classList.toggle('animate');
+}
+
+
+//listener on hamburger btn
+hamburger.addEventListener("click",()=>{
+    moblieMenu.classList.toggle("header__menu--active");
+  if(hamburger.classList.contains("icon-hamburger")){
+      hamburgerOpen();
+      return;
+  }else{
+      hamburgerClose();
+    return;
+  }
+})
+
+/// listener on menu links click 
+headerLinks.forEach(link => {
+  link.addEventListener("click",()=>{
+    if(hamburger.classList.contains("icon-closex")){
+      moblieMenu.classList.toggle("header__menu--active")
+      hamburgerClose();
+    }else{
+      return;
+    }
+  })
+  
+});
+
+
+
+mobileSearchBtn.addEventListener("click",()=>{
+    mobileInput.classList.toggle("header-mobile__input--active");
+});
+
+//desktop
+
+const searchDesktop = document.querySelector(".header-form__btn");
+const inputDesktop = document.querySelector(".header__input");
+
+searchDesktop.addEventListener("click",()=>{
+  inputDesktop.classList.toggle("header__input--active");
+});
